2eee90bdef40f344dd90cf74d7bc8e7bd68a7c47
Niobe <rjsqkd111@gmail.com>
2025-02-12 09:39:44 +0900

    SQL 쿼리 수정 및 새로운 쿼리 추가: 근속 기간 계산, 급여 형식 지정, 급여 별표 표시

--- 20250212.sql
+++ 20250212.sql
@@ -7,6 +7,8 @@ SELECT employee_id, last_name, salary, ROUND(salary*115.5/100) NEW_SALARY
 FROM employees
 ORDER BY employee_id;
 
+    -- 연습문제
+
 /*4. 새 급여에서 이전 급여를 빼는 열을 추가하도록 lab_04_02.sql의 query를 수정합니다.
 열 레이블을 Increase로 지정합니다. 파일 내용을 lab_04_04.sql로 저장합니다. 수정한
 query를 실행합니다.*/
@@ -38,4 +40,28 @@ WHERE last_name LIKE '&Last_name%'
 
 SELECT last_name "Name", LENGTH(last_name) "Length"
 FROM employees
-WHERE UPPER(last_name) LIKE '&Last_name%'
+WHERE LOWER(last_name) LIKE '&Last_name%'
+
+/*6. HR 부서에서 각 사원의 근속 기간을 파악하려고 합니다. 각 사원에 대해 성을 표시하고
+채용일부터 오늘까지 경과한 개월 수를 계산합니다. 열 레이블을 MONTHS_WORKED로
+지정합니다. 재직 개월 수에 따라 결과를 정렬합니다. 개월 수는 가장 가까운 정수로
+반올림해야 합니다.*/
+
+SELECT Last_name, ROUND(MONTHS_BETWEEN(SYSDATE, hire_date)) MONTHS_WORKED
+FROM employees
+ORDER BY MONTHS_WORKED
+
+/*7. 모든 사원의 성과 급여를 표시하기 위한 query를 작성합니다. 급여가 15자 길이로
+표시되고 왼쪽에 $ 기호가 채워지도록 형식을 지정합니다. 열 레이블을 SALARY로
+지정합니다.*/
+
+SELECT LAST_NAME, LPAD(SALARY,15,'$') SALARY
+FROM employees
+
+/*8. 사원의 성을 표시하고 급여 액수를 별표로 나타내는 query를 작성합니다. 각 별표는
+$1,000을 나타냅니다. 급여의 내림차순으로 데이터를 정렬합니다. 열 레이블을
+EMPLOYEES_AND_THEIR_SALARIES로 지정합니다.*/
+
+SELECT LAST_NAME, RPAD('*', TRUNC(SALARY,-3)/1000, '*') EMPLOYEES_AND_THEIR_SALARIES
+FROM employees
+ORDER BY EMPLOYEES_AND_THEIR_SALARIES DESC;
